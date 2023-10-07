1aa5866379d430aa10f90bac0d1deff749837fb1
StoyanDr <stoyan.dragov06@gmail.com>
2023-10-07 12:39:34 +0300

    BackusNaur

--- src/Backus051023.java
+++ src/Backus051023.java
@@ -1,5 +1,22 @@
 public class Backus051023 {
+    public static boolean isWord(String s) {
+        switch(s.length()) {
+            case 0: return false;
+            case 1: return false;
+            case 2: return s.equals("AA") || s.equals("AB");}
+        return s.charAt(0) == 'A' && (s.charAt(s.length() - 1) == 'B' || s.charAt(s.length() - 1) == 'A') && isWord(s.substring(1,s.length() - 1));
+    }
+    //<word>::=C|AB|BA|A<word>|B<word>C
+    public static boolean isWord1(String s) {
+        switch (s.length()) {
+            case 0:return false;
+            case 1: return s.equals("C");
+            case 2: return s.equals("AB")||s.equals("BA")||s.equals("AC");
+        }
+        return (s.charAt(0)=='A'&&isWord1(s.substring(1)))
+                || (s.charAt(0)=='B'&&s.charAt(s.length()-1)=='C'&&isWord1(s.substring(1,s.length()-1)));
+    }
     public static void main(String Args[]){
-        System.out.println("Samo levski");
+
     }
 }
